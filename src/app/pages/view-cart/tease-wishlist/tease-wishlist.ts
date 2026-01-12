f9096aba19b57710efd61b5c6812ec944094ef9a
Yukselcsgn <yukselcosgun@hotmail.com.tr>
2026-01-12 14:35:49 +0300

    basket update

--- src/app/pages/view-cart/tease-wishlist/tease-wishlist.ts
+++ src/app/pages/view-cart/tease-wishlist/tease-wishlist.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ViewPanel } from "../../../directives/view-panel";
+import { MatIcon } from "@angular/material/icon";
+import { EcommerceStore } from '../../../ecommerce-store';
+import { MatButton } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tease-wishlist',
-  imports: [],
+  imports: [ViewPanel, MatIcon, MatButton, RouterLink],
   template: `
-    <p>
-      tease-wishlist works!
-    </p>
+    <div appViewPanel class="flex items-center justify-between">
+      <div class="flex items-center gap-3">
+        <mat-icon class="!text-red-500">favorite_border</mat-icon>
+        <div>
+          <h2 class="text-xl font-bold">Wishlist({{store.wishlistCount()}})</h2>
+          <p class="text-gray-500 text-sm">
+            You Have {{store.wishlistCount()}} items saved for later
+          </p>
+        </div>
+      </div>
+      <div class="flex items-center gap-3">
+        <button matButton routerLink="/wishlist">View All</button>
+        <button matButton="filled" class="flex items-center gap-2" (clicked)="store.addAllWishlistToCart()">
+          <mat-icon>shopping_cart</mat-icon>
+          Add All to Cart
+        </button>
+      </div>
+    </div>
   `,
   styles: ``,
 })
 export class TeaseWishlist {
-
+  store = inject(EcommerceStore);
 }
